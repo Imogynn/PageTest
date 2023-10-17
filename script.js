1331975987ddc8716e4d69659f851c3747e7330f
Chris Desmarais <desmarais_chris@hotmail.com>
2023-10-16 20:55:12 -0600

    Added some data

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("Foo")
+var data = [
+    {name: "Chris", computer: "mac"},
+    {name: "Shane", computer: "pc"},
+    {name: "Tony", computer: "linux"}
+]
+
+function showComputers(){
+    var reportDiv = document.getElementById("report");
+    data.forEach(person => {
+        var newDiv = document.createElement("p")
+        newDiv.textContent = `${person.name} uses a ${person.computer}`
+        reportDiv.appendChild(newDiv)
+    })
+}
+
+showComputers()
